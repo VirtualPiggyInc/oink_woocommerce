023aae1e3ecf9fe3222832425708e30f88572a2a
shtaft <staft@uvic.ca>
2014-06-19 14:09:25 -0700

    Bug Fix
    Bug fix

--- vp-wp-checkout/assets/js/checkout.js
+++ vp-wp-checkout/assets/js/checkout.js
@@ -29,21 +29,12 @@ window.VPCheckout = (function ($) {
 
             self.isLogged(function (isLogged, info) {
                 if (!isLogged) {
-<<<<<<< HEAD
                     self.view.init();
                     self.initEvents();
                     self.view.showLoginBox();
                 } else {
                     self.view.init();
                     self.initEvents();
-=======
-                    self.view.init();
-                    self.initEvents();
-                    self.view.showLoginBox();
-                }  else {
-                    self.view.init();
-                    self.initEvents();
->>>>>>> origin/woocomm-and-shopp-fixes
                     self.afterLogin(isLogged, '', info);
                 }
             });
@@ -245,10 +236,6 @@ window.VPCheckout = (function ($) {
             }
             if (typeof this.data.role === "undefined")
                 this.data = data;
-<<<<<<< HEAD
-=======
-            }
->>>>>>> origin/woocomm-and-shopp-fixes
             $('#payment_method_virtual-piggy').attr('checked', 'checked');
             this.view.hideShippingForm();
             this.view.hidePaymentOptions();
@@ -387,10 +374,6 @@ window.VPCheckout = (function ($) {
             this.cleanBox();
             this.showContentBox()
             $(".virtualpiggy-loginbox").animate({height: "300px", width: "614px"}, 500);
-<<<<<<< HEAD
-=======
-
->>>>>>> origin/woocomm-and-shopp-fixes
             var $fields = $('<div/>')
                 .addClass('virtualpiggy-form')
                 .addClass('virtualpiggy-form-child')
